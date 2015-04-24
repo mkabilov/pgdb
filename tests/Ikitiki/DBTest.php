d1a68c61e8ae6aa8b06f1001493793064f3536bf
Murat Kabilov <murat@kabilov.com>
2015-04-25 00:59:06 +0300

    one more test

--- tests/Ikitiki/DBTest.php
+++ tests/Ikitiki/DBTest.php
@@ -18,6 +18,22 @@ class DBTest extends \PHPUnit_Framework_TestCase {
     }
 
     /**
+     * @return array
+     */
+    public function providerArrays()
+    {
+        return [
+            [['a', 'b', 'c'], '\'{"a","b","c"}\''],
+            [['a', 'olo lo', 'c'], '\'{"a","olo lo","c"}\''],
+            [[], "'{}'"],
+            [["'quoted'", "'"], '\'{"\'\'quoted\'\'","\'\'"}\''],
+            [['"double quoted"', ''], '\'{"\\\"double quoted\\\"",""}\''],
+            [['"double "quoted"', ''], '\'{"\\\"double \\\"quoted\\\"",""}\''],
+            [['{}', '"{}"'], '\'{"{}","\\\"{}\\\""}\''],
+        ];
+    }
+
+    /**
      * @group fast
      *
      * @dataProvider providerStrings
@@ -33,4 +49,19 @@ class DBTest extends \PHPUnit_Framework_TestCase {
         );
     }
 
+    /**
+     * @group fast
+     *
+     * @dataProvider providerArrays
+     *
+     * @param $array
+     * @param $quoted
+     */
+    public function testArrayQuote($array, $quoted)
+    {
+        $this->assertEquals(
+            $quoted,
+            DB::toArray($array)
+        );
+    }
 }
